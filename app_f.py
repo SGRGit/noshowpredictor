116dfd910cde734bd898b5b54675c116055d410d
SGRGit <50324151+SGRGit@users.noreply.github.com>
2019-12-10 20:31:51 +0530

    Add files via upload

---
+++ app_f.py
@@ -0,0 +1,53 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+from datetime import datetime
+
+app = Flask(__name__)
+model = pickle.load(open('model_f.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index_f.html')
+
+@app.route('/predict', methods=['GET', 'POST'])
+def predict():
+    '''
+    For rendering results on PHP GUI
+    '''
+    #int_features = np.zeros((1, 10))
+    #int_features =[int(x) for x in request.form.values()]
+    if request.values.get("Age") == None:
+        age = 0
+    else:
+        age = request.values.get("Age")
+    if request.values.get("Sms Received") == None:
+        sms = 0
+    else:
+        sms = request.values.get("Sms Received")
+    
+    if request.values.get("Appointment Date") == None:
+        appdt = datetime.strptime('1900-01-01', '%Y-%m-%d').date()
+    else:
+        appdt = datetime.strptime(request.values.get("Appointment Date"), '%Y-%m-%d').date()
+    
+    if request.values.get("Scheduled Date") == None:
+        schdt = datetime.strptime('1900-01-01', '%Y-%m-%d').date()
+    else:
+        schdt = datetime.strptime(request.values.get("Scheduled Date"), '%Y-%m-%d').date()
+    
+    deltday = abs((appdt - schdt).days)
+    
+    inp = np.array([age, 0,0 ,0, 0, sms, 0, deltday,0 ,0 ])
+    final_features = np.array(inp).reshape(1, 10)
+    
+    if request.values.get("Age") == None:
+        prediction = 0
+    else:
+        prediction = round(model.predict_proba(final_features)[0][0] *100, 2)
+
+    return render_template('index_f.html', prediction_text = 'Appointment Chance {} %'.format(prediction))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
